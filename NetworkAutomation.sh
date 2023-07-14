5a4cdcc3f2842c7416f5962ac03d243f8008a577
Rhwalters <139407890+Rhwalters@users.noreply.github.com>
2023-07-13 21:55:35 -0500

    Network Automation

---
+++ NetworkAutomation.sh
@@ -0,0 +1,41 @@
+import subprocess
+
+def check_internet_connection():
+    try:
+            # Ping a well-known public DNS server (Google DNS)
+	    subprocess.check_output(['ping', '-c', '3', '8.8.8.8'])
+	    return True
+	except subprocess.CalledProcessError:
+	    return False
+
+def check_dns_resolution():
+	try:
+					            
+# Resolve the hostname of a well-known website (example.com)
+	subprocess.check_output(['nslookup', 'example.com'])
+	return True
+except subprocess.CalledProcessError:
+	return False
+
+def check_website_access(url):
+	try:
+	# Access the provided URL
+	subprocess.check_output(['curl', '-I', url])
+	return True
+except subprocess.CalledProcessError:
+	return False
+
+									
+def main():
+										print("Network Troubleshooting Script")
+    print("=============================")
+
+										# Check internet connection
+    print("Checking internet connection...")
+										if check_internet_connection():
+	print("Internet connection: OK")
+    else:
+	print("Internet connection: Failed")
+
+																						 
+main()
